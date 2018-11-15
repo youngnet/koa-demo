a23ef43ec6224ec4593b838c6c36dd2c273f0b42
heheyoung <yangyang@yunjianvc.com>
2018-11-15 19:10:46 +0800

    pull

--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 const koa = require('koa');
 const path = require("path");
 const mysql = require("mysql");
-console.log('<-  log ->  path', path.join(__dirname,'../../react-demo'));
-
+const koaSession = require("koa-session");
 
 import router from './router';
 const app = new koa();
+app.keys = ['some secret hurr'];
+const CONFIG = {
+  key: 'koa:sess', /** (string) cookie key (default is koa:sess) */
+  /** (number || 'session') maxAge in ms (default is 1 days) */
+  /** 'session' will result in a cookie that expires when session/browser is closed */
+  /** Warning: If a session cookie is stolen, this cookie will never expire */
+  maxAge: 86400000,
+  autoCommit: true, /** (boolean) automatically commit headers (default true) */
+  overwrite: true, /** (boolean) can overwrite or not (default true) */
+  httpOnly: true, /** (boolean) httpOnly or not (default true) */
+  signed: true, /** (boolean) signed or not (default true) */
+  rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
+  renew: false, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+};
 
 const connection = mysql.createConnection({
   host     : '127.0.0.1',   // 数据库地址
@@ -17,13 +30,13 @@ const connection = mysql.createConnection({
 
 let data = null; 
 // 执行sql脚本对数据库进行读写 
-
+app.use(koaSession(CONFIG, app));
 (async ()=>{
   data = await getData();
   console.log(data,'---data---')
   app.use((ctx,next)=>{
-	ctx.request.data = data;
-	next();
+    ctx.request.data = data;
+    next();
   })
   app.use(router.routes());
 
