594192123e6a366dcdba269cdb2fd30f63fd01e5
heheyoung <yangyang@yunjianvc.com>
2018-12-03 16:30:49 +0800

    return_file

--- server/router/index.js
+++ server/router/index.js
@@ -1,19 +1,37 @@
-const router = require("koa-router")();
+const router = require('koa-router')();
 import test from './test';
 const fs = require('fs');
 const path = require('path');
+const baseUrl = path.join(__dirname, '../../../react-demo/build');
 
 router.use((ctx, next) => {
-    next();
-})
-router.get('/home',async (ctx, next) => {
-    let data = fs.readFileSync('/Users/admin/young-web/stock-pro/react-demo/build/index.html','utf8');
-    ctx.body = data;
-})
+	console.log('​ctx -> ', ctx.request.url);
+	next();
+});
 
-router.get('/static/*',async (ctx,next) => {
-    ctx.body = fs.readFileSync(`/Users/admin/young-web/stock-pro/react-demo/build${ctx.request.url}`,'utf8');
-})
-router.use('/test',test.routes());
+router.redirect('/', '/home');
+
+router.get('/home', async (ctx, next) => {
+	let data = fs.readFileSync(`${baseUrl}/index.html`, 'utf8');
+	ctx.type = 'text/html';
+	ctx.body = data;
+});
+
+router.get('/favicon.ico', async (ctx, next) => {
+	ctx.body = fs.readFileSync(`${baseUrl}/favicon.ico`);
+});
+
+router.get('/static/*', async (ctx, next) => {
+	let data = fs.readFileSync(`${baseUrl}${ctx.request.url}`, 'utf8');
+	if (/\.js$/.test(ctx.request.url)) {
+		ctx.type = 'text/javascript';
+	}
+	if (/\.css$/.test(ctx.request.url)) {
+		ctx.type = 'text/css';
+	}
+	ctx.body = data;
+});
+
+router.use('/test', test.routes());
 
 export default router;
